b49f9da27053fb68b2185cd954920214fbf0fb63
El Codigo Dominicano <Anburivas@gmail.com>
2022-07-16 23:20:54 -0400

    Update app.py

--- the_factory/app.py
+++ the_factory/app.py
@@ -1,102 +1,72 @@
-import platform
 import sys
 from the_factory import thefactory
 
-WINDOWS_ACCEPTED_ARGS = [
-    'bus',
-    'processor',
-    'network',
-    'memory',
-    'drivers',
-    'sound',
-    'usb',
-    'floppy',
-    'ide',
-    'pcmcia',
-    'usb',
-    'parallel',
-    'serial',
-    'graphics',
-    ]
-LINUX_ACCEPTED_ARGS = [
-    'bios',
-    'processor',
-    'network',
-    'memory',
-    'drivers',
-    'check_vuln',
-    'power',
-    '',
-    '',
-    '',
-    '',
-    '',
-    ]
-operating_system = thefactory.get_os()
 
 def main(args: list[str]):
+    operating_system = thefactory.get_os()
+    print(f"The following arguments for the {operating_system} system:", *thefactory.WINDOWS_ACCEPTED_ARGS, sep=" ")
+    print()
     if operating_system == 'Windows':
-        print(f"The following commands are for the {operating_system} system:", *WINDOWS_ACCEPTED_ARGS, sep=" ")
         w = thefactory.WinRig()
-        for arguement in args:
+        for argument in args:
             try:
-                if arguement == 'bus':
+                if argument == 'bus':
                     w.get_bus_information()
-                if arguement == 'processor':
+                if argument == 'processor':
                     w.get_processor_information()
-                if arguement == 'network':
+                if argument == 'network':
                     w.get_network_information()
-                if arguement == 'memory':
+                if argument == 'memory':
                     w.get_memory_information()
-                if arguement == 'drivers':
+                if argument == 'drivers':
                     w.get_driver_information()
-                if arguement == 'sound':
+                if argument == 'sound':
                     w.get_sound_device_information()
-                if arguement == 'usb':
+                if argument == 'usb':
                     w.get_usb_port_information()
-                if arguement == 'floppy':
+                if argument == 'floppy':
                     w.get_floppy_drive_information()
-                if arguement == 'ide':
+                if argument == 'ide':
                     w.get_ide_controller_information()
-                if arguement == 'pcmcia':
+                if argument == 'pcmcia':
                     w.get_pcmcia_controller_information()
-                if arguement == 'usb':
+                if argument == 'usb':
                     w.get_usb_port_information()
-                if arguement == 'parallel':
+                if argument == 'parallel':
                     w.get_parallel_port_information()
-                if arguement == 'serial':
+                if argument == 'serial':
                     w.get_serial_port_information()
-                if arguement == 'graphics':
+                if argument == 'graphics':
                     w.get_graphic_card_information()
-                if arguement not in WINDOWS_ACCEPTED_ARGS:
-                    print(f"The provided arguement [{arguement}] is invalid, please run again.")
+                if argument not in thefactory.WINDOWS_ACCEPTED_ARGS:
+                    print(f"The provided argument [{argument}] is invalid, please run again.")
                     raise
             finally:
                 sys.exit(0)
-     
+        
     if operating_system == 'Linux':
-        print(f"The following commands are for the {operating_system} system:", *LINUX_ACCEPTED_ARGS, sep=" ")
+        print(f"The following commands for the {operating_system} system:", *thefactory.LINUX_ACCEPTED_ARGS, sep=" ")
         x = thefactory.NixRig()
-        for arguement in args:   
-            try:     
-                if arguement == 'bios':
-                    x.get_smbios_information()
-                if arguement == 'processor':
-                    x.get_processor_information()
-                if arguement == 'network':
-                    x.get_network_information()
-                if arguement == 'memory':
-                    x.get_memory_information()
-                if arguement == 'drivers':
-                    x.get_driver_information()
-                if arguement == 'check_vuln':
-                    x.get_cpu_vuln_information()
-                if arguement == 'power':
-                    x.get_power_information()
-                if arguement not in LINUX_ACCEPTED_ARGS:
-                    print(f"The Provided arguement [{arguement}] is invalid, please run again.")
-            finally:
-                sys.exit(0)
+    for argument in args:   
+        try:     
+            if argument == 'bios':
+                x.get_smbios_information()
+            if argument == 'processor':
+                x.get_processor_information()
+            if argument == 'network':
+                x.get_network_information()
+            if argument == 'memory':
+                x.get_memory_information()
+            if argument == 'drivers':
+                x.get_driver_information()
+            if argument == 'check_vuln':
+                x.get_cpu_vuln_information()
+            if argument == 'power':
+                x.get_power_information()
+            if argument not in thefactory.LINUX_ACCEPTED_ARGS:
+                print(f"The Provided argument [{argument}] is invalid, please run again.")
+        finally:
+            sys.exit(0)
 
     if operating_system == 'Darwin':
         print(f"Implementation coming soon for the {operating_system} environment")
